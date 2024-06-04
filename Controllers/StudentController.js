11a0af26c371e238290313bbbc97c4ced794656c
chitra0404 <chitrasuresh0404@gmail.com>
2024-06-04 20:14:27 +0530

    commit

--- Controllers/StudentController.js
+++ Controllers/StudentController.js
@@ -47,7 +47,7 @@ module.exports.StudentRegister=async(req,res)=>{
 }
 
 module.exports.studentLogin=async(req,res)=>{
-    // try{
+    try{
         const {studentID,email,password}=req.body;
         let student=await Student.findOne({ studentID, email })
         if(!student){
@@ -58,7 +58,7 @@ module.exports.studentLogin=async(req,res)=>{
             return res.status(409).json({message:"invalid password"}); }
         if(student && passwordmatch){
               
-            const token=jwt.sign({ studentID:student.studentID},process.env.Student_KEY,{expiresIn:'24hr'})
+            const token=jwt.sign({ studentID:student.studentID},process.env.STUDENT_KEY,{expiresIn:'24hr'})
                 res.json({
                  
                   
@@ -71,11 +71,11 @@ module.exports.studentLogin=async(req,res)=>{
 
         }
 
-    // }
+    }
    
     
     
-    // catch (err) {
-    //     res.status(500).json(err);
-    // }
+    catch (err) {
+        res.status(500).json(err);
+    }
 }
